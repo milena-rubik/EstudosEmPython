17b572544b149ebe0a2224f6d177bbd161d39ec4
milena-rubik <milenarubik95@gmail.com>
2021-06-26 16:24:17 -0300

    versaocorrigida

--- Estudos01/01variaveis.py
+++ Estudos01/01variaveis.py
@@ -1,8 +1,9 @@
-brigadeiro = input('digite a quantia de brigadeiro ') 
-beijinho = input('digite o número de beijinho ')
-coxinha = input('digite o número de coxinha ')
-convidados = input('digite o número de convidados ')
-soma = brigadeiro + beijinho
+brigadeiro = int(input('digite a quantia de brigadeiro ')) 
+beijinho = int(input('digite o número de beijinho '))
+coxinha = int(input('digite o número de coxinha '))
+convidados = int(input('digite o número de convidados '))
+soma = brigadeiro + beijinho 
+#caso não tivéssemos especificado 'int' os números seriam reconhecidos como string e o resultado seria junção visual deles e não a soma
 
 if coxinha < convidados or soma < convidados:
     print('certeza que vai dar quebra pau')
@@ -12,5 +13,6 @@ else:
    print('me chama que eu vou')
 
 print('Pense bem se vai chamar {} convidados!'.format(convidados))
+print('terá {} docinhos.'.format(soma))
 
 #no VS Code se tentar só 'run code' o input não vai funcionar, é necessário selecionar o 'Run Python File in Terminal' 
