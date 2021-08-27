356ec10f16b7ec8894d40b28e10a54ca137ddf9e
Milena Rubik Peres <56029188+milena-rubik@users.noreply.github.com>
2021-08-26 22:15:12 -0300

    Update Questao1.py

--- ExerciciosInfinet/TP3/Questao1.py
+++ ExerciciosInfinet/TP3/Questao1.py
@@ -16,7 +16,12 @@ Informe o valor total do consumo: R$ 100.00
 Informe o total de pessoas: 2
 Informe o percentual do serviço, entre 0 e 100: 10
 O valor total da conta, com a taxa de serviço, será de R$ 110,00.
-Dividindo a conta por 2 pessoa(s), cada pessoa deverá pagar R$ 55,00. """
+Dividindo a conta por 2 pessoa(s), cada pessoa deverá pagar R$ 55,00. """ 
+
+def conversorentrada(entrada): #função criada para permitir também o uso da vírgula como separador decimal
+    entrada = entrada.replace(',','.')
+    return entrada
+
 
 def conversorsaida(saida): #define método 
     saida = format(saida, '.2f') #formata para exibir 2 casas decimais
@@ -24,33 +29,40 @@ def conversorsaida(saida): #define método
     saida = saida.replace('.',',') #substitui . por ,
     return saida #retorna a variável modificada pelo método
 
-print('Alerta! Instruções de uso\n 1- Use ponto como separador decimal\n 2- Não use separador de milhar')
+print('Alerta! Não use separador de milhar!')
 
-try: #try - except traz erro de entrada caso a entrada fornecida não possa ser executada pelo bloco dentro do try
-    valorconta = float(input('Informe o valor total de consumo: R$  '))
+try: #'try - except' executa o except caso a entrada fornecida não possa ser executada pelo bloco do try
+    valorconta = input('Informe o valor total de consumo: R$  ')
+    valorconta = float(conversorentrada(valorconta))
+    if valorconta <0:
+        raise Exception
 except:
+    print('Valor inválido')
+    exit() #dá ordem para fechar o programa, como requisitado que ocorresse em caso de erro no enunciado da questão
+
+try:
+    numeropessoas = input('Informe o total de pessoas: ') #captura entrada para a variável definindo que o tipo será inteiro
+    numeropessoas = int(conversorentrada(numeropessoas))
+    if  numeropessoas <= 0 or not type(numeropessoas) == int: #Define que se a variável for <= zero ou não for do tipo inteira cairá na exceção abaixo
+        raise Exception #gera exceção se não atender a condição acima
+except: 
     print('Valor inválido')
     exit()
 
 try:
-    numeropessoas = int(input('Informe o total de pessoas: ')) #captura entrada para a variável definindo que o tipo será inteiro
-    if  numeropessoas <= 0 or not type(numeropessoas) == int: #Define que se a variável for <= zero ou não for do tipo inteira cairá na exceção abaixo
-        raise Exception #cria exceção se o número estiver fora do intervalo 
-except: #se valor invalido para inteiro ou fora do intervalo, executa abaixo
+    taxaservico = input('Informe o percentual do serviço entre 0 e 100: ')
+    taxaservico = float(conversorentrada(taxaservico))
+    if taxaservico < 0 or taxaservico > 100:
+         raise Exception
+except:
     print('Valor inválido')
     exit()
 
-taxaservico = float(input('Informe o percentual do serviço entre 0 e 100: '))
-if taxaservico < 0 or taxaservico > 100:
-    print('Valor inválido')
-    exit()
-
-valortotal = round((((taxaservico/100)+1)*valorconta),2)
+valortotal = round((((taxaservico/100)+1)*valorconta),2) #traz o valor total da conta incluíndo a taxa de serviço arredondado em 2 casas decimais
 
 from math import floor #floor serve para arredondar para baixo
-porpessoa = ((floor((valortotal/numeropessoas)*100)))/100 #gambiarra, como o floor só arredonda para inteiro multipliquei por 100 para tornar inteiras as casas decimais de interesse, usar floor e dividir 100 para retornar o numero só que arredondado
-print(porpessoa)
-resto = round(valortotal - (porpessoa*numeropessoas),2)
+porpessoa = ((floor((valortotal/numeropessoas)*100)))/100 #como o floor só arredonda para n° inteiro e preciso de 2 casas decimais, multipliquei por 100 para tornar inteiras as casas decimais de interesse, usar floor e dividir 100 para retornar o numero só que arredondado
+resto = round(valortotal - (porpessoa*numeropessoas),2) 
 umapessoa = porpessoa + resto
 
 valortotal = conversorsaida(valortotal)
@@ -64,4 +76,15 @@ if resto == 0:
 else:
     print(f'Dividindo a conta por {numeropessoas} pessoa(s), uma pessoa deverá pagar R$ {umapessoa} e cada uma das demais pessoas R$ {porpessoa}')
 
-    
+#PARTE BÔNUS
+if resto != 0: #Se resto diferente de zero
+    listaparticipante = list()
+    numeroparticipantes = int(input('Se você deseja que quem irá pagar o maior valor seja sorteado digite o número de participantes desse sorteio '))
+    for x in range (1,numeroparticipantes + 1): #percorre o código abaixo no intervalo selecionado
+        participante = input(f'Digite o nome do {x} participante ')
+        listaparticipante.append(participante) #adiciona o nome digitado a posição 'x' em participante
+    import random
+    print(f'O Azarado a pagar a sua parte mais o restante da conta é {random.choice(listaparticipante)}')
+else:
+    print('Podem brigar!')
+    exit()
