37513a6e0d2deb12b43227d6e1afce80386b1f55
Milena Rubik Peres <56029188+milena-rubik@users.noreply.github.com>
2021-08-26 21:28:03 -0300

    Update Questao3.py

--- ExerciciosInfinet/TP3/Questao3.py
+++ ExerciciosInfinet/TP3/Questao3.py
@@ -11,25 +11,24 @@ for x in range (1,4): #percorre o código abaixo no intervalo selecionado
     listaparticipante.append(participante) #adiciona o nome digitado a posição 'x' em participante
     while True: #Executa enquanto for verdadeiro, como por padrão é verdadeiro, a intenção é ficar repetindo em caso de 
         #entrada inválida até que seja digitado entrada válida caindo no 'else' e 'while' seja interrompido pelo break
-        nota = float(input(f'Digite a nota do {x} participante '))
+        nota = float(input(f'Digite a nota do {x} participante'))
         if nota <0 or nota > 10 or not type(nota) == float:
-            print('Entrada inválida')
+            print('Entrada inválida. O Valor deve estar entre 0 e 10, se precisar utilize "." como separador decimal')
         else:
             listanota.append(nota)
             break
 
 maiornota = max(listanota) #captura o maior valor na lista
-indicevencedor = listanota.index(max(listanota))
+indicevencedor = listanota.index(max(listanota)) #captura posição da maior nota na lista
 
 listanotaempate = [i for i, item in enumerate(listanota) if item == maiornota] #captura índices que forem iguais a maior nota na lista notas
 
-if listanota.count(maiornota) > 1:
-    print('Houve empate entre os participantes: ')
+if listanota.count(maiornota) > 1: #conta quantas vezes o valor da maior nota se repete, caso maior que 1 executa o comando abaixo
+    print(f'Houve empate! Os participantes listados abaixo obteram nota {maiornota} ')
     for x in listanotaempate:
         print(listaparticipante[x])
 else:
      print(f'O vencedor é {listaparticipante[indicevencedor]} com nota {listanota[indicevencedor]}')
-
 
 
 
